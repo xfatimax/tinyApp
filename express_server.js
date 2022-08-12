e291cca6a22cf014bd979c853575937adca93d0a
Fatima <xfatimakx@gmail.com>
2022-08-12 22:27:43 +0000

    CURL implemented

--- express_server.js
+++ express_server.js
@@ -18,3 +18,9 @@ app.listen(PORT, () => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+curl -i http://localhost:8080/hello
