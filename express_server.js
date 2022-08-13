6cf1dd6279e5e1076059c1a492dcfb72f4d76a6c
Fatima <xfatimakx@gmail.com>
2022-08-13 03:04:46 +0000

    Second route and template added

--- express_server.js
+++ express_server.js
@@ -29,3 +29,8 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase};
+  res.render("urls_show", templateVars);
+});
