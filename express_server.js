db3c79b1efbe9ff718f278070f21c75a3f86b338
Fatima <xfatimakx@gmail.com>
2022-08-23 19:36:12 +0000

    Registration Handler + Header updates

--- express_server.js
+++ express_server.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const generateRandomString = function() {
+  return Math.random().toString(36).slice(2, 8);
+};
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+    b6UTxQ: {
+      longURL: "https://www.tsn.ca",
+      userID: "aJ48lW"
+    },
+    i3BoGr: {
+      longURL: "https://www.google.ca",
+      userID: "aJ48lW"
+    }
+  };
+
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
 };
 
 app.use(express.urlencoded({ extended: true }));
@@ -27,82 +48,17 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase};
-  res.render("urls_show", templateVars);
-  res.redirect(longURL)
-});
-
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  urlDatabase[generateRandomString()] = req.body.longURL;
-  //res.send("Ok"); // Respond with 'Ok' (we will replace this)
-  res.redirect(`/urls/${generateRandomString()}`);
-});
-
-const generateRandomString = function() {
-  return Math.random().toString(36).slice(2, 8);
-};
-
-app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.longURL];
-  res.redirect(longURL);
-});
-
-app.post("/urls", (req, res) => {
-  const userID = req.session.user_id;
-  if (req.session.user) {
-    urlsForUser(urlDatabase, userID); 
-    urlDatabase[generateRandomString()] = {longURL: req.body.longURL, userID: req.session.user_id}; 
-    res.redirect(`/urls/${generateRandomString()}`); // Redirects to new url's page
-  }
-})
-
-app.post("/login", (req, res) => {
-  const userEmail = req.body.email;
-  const userPassword = req.body.password; // For testing with bcrypt.compareSync. Checks if input matches value in database
-  // const userPresence = emailPwdMatch(users, userEmail, userPassword);
-  // const foundUserID = getUserID(users, req.body.email);
-});
-
-const templateVars = {
-  username: req.cookies["username"],
-  // ... any other vars
-};
-res.render("urls_index", templateVars);
-
-app.post("/register", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-
-  if (email === "" || password === "") {
-    res.status(400).send('Email and password are required!');
-
-  }
-  if (getUserByEmail(email, users)) {
-    res.status(400).send('Email is already registered!');
-
-  } else {
-    const userID = newUser(email, password, users);
-    req.session.userID = userID;
-    res.redirect("/urls");
-  }
+  const longURL = req.body.longURL;
+  const userID = req.session.userID;
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/register", (req, res) => {
   const user = req.session.userID;
-  const templateVars = {
-    user: req.session.userID
-  };
+  const templateVars = {user: req.session.userID};
   if (user) {
     res.redirect('urls');
 
@@ -110,3 +66,98 @@ app.get("/register", (req, res) => {
     res.render('register', templateVars);
   }
 });
+
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const userID = newUser(email, password, users);
+  req.session.userID = userID;
+  res.redirect("/urls");
+});
+
+app.get('/login', (req, res) => {
+  const user = req.session.user;
+  const templateVars = {user: req.session.userID,
+  };
+  if (user) {
+    res.redirect('urls');
+  } else {
+    res.render('login', templateVars);
+  }
+});
+
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password; 
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const user = getUserByEmail(email, users);
+
+  if (!user) {
+    res.status(400).send("Email is not registered");
+
+  }
+  if (!bcrypt.compareSync(password, hashedPassword)) {
+    res.status(400).send("Password is incorrect");
+  
+  } else {
+
+    res.redirect("urls");
+  }
+
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { user: users[req.session.user], urls: urlsForUser(req.session.user, urlDatabase) };
+  res.render('urls_index', templateVars);
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.longURL];
+  if (!urlDatabase[req.params.id]) {
+    res.status(400).send('URL is not in the database');
+  }
+  res.redirect(longURL);
+});
+
+app.get("/urls/:id", (req, res) => {
+
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id], 
+    user: users[req.session.user_id]};
+  
+  if (req.session.user_id === users.userID) {
+    res.render('urls_show', templateVars);
+  }
+  res.status(400).send('This page does not belong to you!');
+});
+
+app.get("/urls/new", (req, res) => {
+  const templateVars = { user: req.session.user, userID: req.session.user_id};
+  
+  if (templateVars.user) {
+    res.render('urls_new', templateVars);
+
+  } else {
+    res.redirect('/login');
+  }
+});
+
+
+
+app.post("/urls/:id", (req, res) => {
+  if (urlDatabase[req.params.id].userID === req.session["userID"]) {
+    let longURL = req.body.longURL;
+    urlDatabase[req.params.id].longURL = longURL;
+    res.redirect('/urls');
+  } else {
+    res.status(400).send("Permission denied");
+  }
+});
+
+app.delete("urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+});
+
+
